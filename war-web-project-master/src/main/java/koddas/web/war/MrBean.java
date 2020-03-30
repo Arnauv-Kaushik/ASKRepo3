eff662d01c44f2bd95175fe047fc0a60449cb572
Arnauv-Kaushik <61408068+Arnauv-Kaushik@users.noreply.github.com>
2020-03-30 12:19:04 +0530

    Update MrBean.java

--- war-web-project-master/src/main/java/koddas/web/war/MrBean.java
+++ war-web-project-master/src/main/java/koddas/web/war/MrBean.java
@@ -4,6 +4,6 @@ public class MrBean
 	
 	public static void main(String...args)
 	{
-	System.out.println("HI from ASK !! Mr.Bean.java");	
+	System.out.println("HI #2 from ASK !! Mr.Bean.java");	
 	}
 }
